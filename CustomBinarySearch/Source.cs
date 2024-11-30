964dd1c8a062915f815df686e3c13d9497bf9c39
lbrooks81 <143453730+lbrooks81@users.noreply.github.com>
2024-11-30 15:10:40 -0500

    Removed name from top of source file

--- CustomBinarySearch/Source.cs
+++ CustomBinarySearch/Source.cs
@@ -1,9 +1,4 @@
-﻿/*
- * Name: [YOUR NAME HERE]
- * South Hills Username: [YOUR SOUTH HILLS USERNAME HERE]
- */
-
-namespace CustomBinarySearch
+﻿namespace CustomBinarySearch
 {
     public static class Source
     {
