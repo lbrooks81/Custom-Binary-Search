a8defe8ce501dba4519d6570f6a115c9e5cc7681
lbrooks81 <143453730+lbrooks81@users.noreply.github.com>
2024-11-30 14:46:12 -0500

    Add files via upload

---
+++ CustomBinarySearch/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomBinarySearch
+{
+	public class Program
+	{
+		private const uint ARRAY_SIZE = 16;
+
+		//DO NOT MODIFY THE MAIN METHOD
+		public static void Main()
+		{
+			Random rand = new Random();
+			int[] randNums = new int[ARRAY_SIZE];
+
+			for (int i = 0; i < randNums.Length; i++)
+			{
+				int randNum = rand.Next(0, 100);
+
+				while (randNums.Contains(randNum))
+				{
+					randNum = rand.Next(0, 100);
+				}
+
+				randNums[i] = randNum;
+			}
+
+			Array.Sort(randNums);
+
+			Console.WriteLine($"Numbers generated {String.Join(", ", randNums)}");
+			Console.Write("Enter a number to search for: ");
+			int searchNum = int.Parse(Console.ReadLine()!);
+
+			int index = Source.BinarySearch(randNums, searchNum);
+
+			if(index != -1)
+			{
+				Console.WriteLine($"Number found at index {index}");
+                if (randNums[index] != searchNum)
+                {
+					PrintIncorrectMsg();
+                }
+				else
+				{
+					PrintCorrectMsg();
+                }
+            }
+			else
+			{
+				Console.WriteLine("Number not found");
+				if(randNums.Contains(searchNum))
+				{
+					PrintIncorrectMsg();
+                }
+				else
+				{
+					PrintCorrectMsg();
+                }
+			}
+		}
+
+		private static void PrintIncorrectMsg()
+		{
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Error.WriteLine("BinarySearch method is not implemented correctly");
+            Console.ResetColor();
+        }
+
+		private static void PrintCorrectMsg()
+		{
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("This result is correct");
+            Console.ResetColor();
+        }
+	}
+}
---
+++ CustomBinarySearch/Source.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Name: [YOUR NAME HERE]
+ * South Hills Username: [YOUR SOUTH HILLS USERNAME HERE]
+ */
+
+namespace CustomBinarySearch
+{
+    public static class Source
+    {
+        public static int BinarySearch(int[] arr, int match)
+        {
+            int left = 0;
+            int right = arr.Length - 1;
+
+            while (right >= left)
+            {
+                int mid = left + (right - left) / 2;
+
+                if (arr[mid] == match)
+                {
+                    return mid;
+                }
+                else if (arr[mid] > match)
+                {
+                    right = mid - 1;
+                }
+                else if (arr[mid] < match)
+                {
+                    left = mid + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
